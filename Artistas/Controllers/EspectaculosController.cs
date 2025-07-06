2178eb83253d027b32e5e758d020e35eb9adca1b
Fernando <fernandobonilla832@gmail.com>
2025-07-06 16:44:28 -0300

    Espectaculo - Agregado metodo PUT en espectaculos

--- Artistas/Controllers/EspectaculosController.cs
+++ Artistas/Controllers/EspectaculosController.cs
@@ -50,25 +50,36 @@ namespace Artistas.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Espectaculo> GetEspectaculo(int id)
+        public ActionResult<RespuestaEspectaculoDTO> GetEspectaculo(int id)
         {
             if (id <= 0)
             {
                 return BadRequest("El Id no puede ser menor o igual a cero");
             }
 
-            Espectaculo? espectaculo = _context.Espectaculos.FirstOrDefault(e => e.Id == id);
+            Espectaculo? espectaculo = _context.Espectaculos
+                .Include(e => e.Artista)
+                .FirstOrDefault(e => e.Id == id);
 
             if (espectaculo == null)
             {
                 return NotFound($"Espectaculo con id: {id} no existe");
             }
 
-            return Ok(espectaculo);
+            // Ahora siguiendo solo mostramos los datos que queremos a travez del DTO
+            RespuestaEspectaculoDTO espectaculoResp = new RespuestaEspectaculoDTO();
+            espectaculoResp.Id = espectaculo.Id;
+            espectaculoResp.Titulo = espectaculo.Titulo ?? "";
+            espectaculoResp.Fecha = espectaculo.Fecha;
+            espectaculoResp.Horario = espectaculo.Horario;
+            espectaculoResp.ArtistaId = espectaculo.ArtistaId ?? 0;
+            espectaculoResp.NombreArtista = espectaculo.Artista?.Nombre ?? "";
+
+            return Ok(espectaculoResp);
         }
 
         [HttpPost]
-        public ActionResult<Espectaculo> PostEspectaculo([FromBody] Espectaculo espectaculo)
+        public ActionResult<RespuestaEspectaculoDTO> PostEspectaculo([FromBody] EspectaculoDTO espectaculo)
         {
             // Validaciones de usuario por ahora nada de eso
 
@@ -89,17 +100,73 @@ namespace Artistas.Controllers
                 return BadRequest($"No existe artista con id: {espectaculo.ArtistaId}");
             }
 
+            // Creamos el objeto espectaculo usando los datos que pedimos con el EspectaculoDTO
             Espectaculo nuevoEspectaculo = new Espectaculo { };
             nuevoEspectaculo.Titulo = espectaculo.Titulo;
-            nuevoEspectaculo.Fecha = espectaculo.Fecha;
-            nuevoEspectaculo.Horario = espectaculo.Horario;
-            nuevoEspectaculo.ArtistaId = espectaculo.ArtistaId;
+            nuevoEspectaculo.Fecha = DateOnly.Parse(espectaculo.Fecha);
+            nuevoEspectaculo.Horario = TimeOnly.Parse(espectaculo.Horario);
+            nuevoEspectaculo.ArtistaId = espectaculo.ArtistaId;            
 
             _context.Espectaculos.Add(nuevoEspectaculo);
             _context.SaveChanges();
 
-            return Ok(nuevoEspectaculo);
+            // Y mostramos el resultado usando el DTO de respuesta
+            RespuestaEspectaculoDTO espectaculoResp = new RespuestaEspectaculoDTO();
+            espectaculoResp.Id = nuevoEspectaculo.Id;
+            espectaculoResp.Titulo = nuevoEspectaculo.Titulo ?? "";
+            espectaculoResp.Fecha = nuevoEspectaculo.Fecha;
+            espectaculoResp.Horario = nuevoEspectaculo.Horario;
+            espectaculoResp.ArtistaId = nuevoEspectaculo.ArtistaId ?? 0;
+            espectaculoResp.NombreArtista = artista.Nombre ?? "";
+
+            return Ok(espectaculoResp);
             
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<RespuestaEspectaculoDTO> PutEspectaculo(int id, [FromBody] EspectaculoDTO parametrosEspectaculo)
+        {
+            if(parametrosEspectaculo == null)
+            {
+                return BadRequest("El body del request estaba vacio");
+            }
+
+            Espectaculo? espectaculo = _context.Espectaculos.FirstOrDefault(e => e.Id == id);
+            if (espectaculo == null)
+            {
+                return NotFound($"No existe espectaculo con id: {id}");
+            }
+
+            if (ModelState.IsValid == false)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Chequeamos que si el capo deja algunos campos vacios al modificar, toma por defecto los que ya tiene
+            espectaculo.Titulo = string.IsNullOrEmpty(parametrosEspectaculo.Titulo) ? espectaculo.Titulo : parametrosEspectaculo.Titulo;
+            espectaculo.Fecha = string.IsNullOrEmpty(parametrosEspectaculo.Fecha) ? espectaculo.Fecha : DateOnly.Parse(parametrosEspectaculo.Fecha);
+            espectaculo.Horario = string.IsNullOrEmpty(parametrosEspectaculo.Horario) ? espectaculo.Horario : TimeOnly.Parse(parametrosEspectaculo.Horario);
+            espectaculo.ArtistaId = (parametrosEspectaculo?.ArtistaId == null || parametrosEspectaculo?.ArtistaId == 0) ? espectaculo.ArtistaId : parametrosEspectaculo.ArtistaId;
+
+            // Armamos el DTO de respuesta
+            RespuestaEspectaculoDTO espectaculoResp = new RespuestaEspectaculoDTO();
+            espectaculoResp.Id = espectaculo.Id;
+            espectaculoResp.Titulo = espectaculo.Titulo ?? "";
+            espectaculoResp.Fecha = espectaculo.Fecha;
+            espectaculoResp.Horario = espectaculo.Horario;
+            espectaculoResp.ArtistaId = espectaculo.ArtistaId ?? 0;
+            espectaculoResp.NombreArtista = espectaculo?.Artista?.Nombre ?? "";
+
+            try
+            {
+                //_context.Espectaculos.Update(espectaculo); 
+                _context.SaveChanges();
+                return Ok(espectaculoResp);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
---
+++ Artistas/Models/DTOs/EspectaculoDTO.cs
@@ -0,0 +1,14 @@
+﻿using Artistas.Models;
+
+namespace Artistas.Models.DTOs
+{
+    public class EspectaculoDTO
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string? Fecha {  get; set; }
+        public string? Horario { get; set; }
+        public int? ArtistaId { get; set; }
+    }
+}
+
+
